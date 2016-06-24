bc8f8e2b35ce38984175db4f460db2cae6d59322
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2016-06-24 08:58:12 +0100

    Cambridge Junction events

--- src/main/scala/uk/vitalcode/events/Pages.scala
+++ src/main/scala/uk/vitalcode/events/Pages.scala
@@ -1,8 +1,11 @@
 package uk.vitalcode.events
 
-import uk.vitalcode.events.cambridge.CambridgeScienceCentre
+import uk.vitalcode.events.cambridge.{CambridgeScienceCentre, Junction}
 import uk.vitalcode.events.model.Page
 
 object Pages {
-    val all: Set[Page] = Set(CambridgeScienceCentre.page)
+    val all: Set[Page] = Set(
+        CambridgeScienceCentre.page,
+        Junction.page
+    )
 }
