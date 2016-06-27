6fbe0dd76b916344e25389125c15738372768362
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2016-06-27 18:15:09 +0100

    Add visit cambridge site mappings

--- src/main/scala/uk/vitalcode/events/Pages.scala
+++ src/main/scala/uk/vitalcode/events/Pages.scala
@@ -1,12 +1,12 @@
 package uk.vitalcode.events
 
-import uk.vitalcode.events.cambridge.{CambridgeScienceCentre, Junction, Ballare}
+import uk.vitalcode.events.cambridge.{CambridgeScienceCentre, Junction, VisitCambridge}
 import uk.vitalcode.events.model.Page
 
 object Pages {
     val all: Set[Page] = Set(
         CambridgeScienceCentre.page,
         Junction.page,
-        Ballare.page
+        VisitCambridge.page
     )
 }
