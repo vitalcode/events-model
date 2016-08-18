9069c6b8d7eb06de49309d272e24cc2c49dc5e68
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2016-08-18 21:38:39 +0100

    Adc Theatre page model

--- src/main/scala/uk/vitalcode/events/Pages.scala
+++ src/main/scala/uk/vitalcode/events/Pages.scala
@@ -1,12 +1,13 @@
 package uk.vitalcode.events
 
-import uk.vitalcode.events.cambridge.{CambridgeScienceCentre, Junction, VisitCambridge}
+import uk.vitalcode.events.cambridge.{AdcTheater, CambridgeScienceCentre, Junction, VisitCambridge}
 import uk.vitalcode.events.model.Page
 
 object Pages {
     val all: Set[Page] = Set(
         CambridgeScienceCentre.page,
         Junction.page,
-        VisitCambridge.page
+        VisitCambridge.page,
+        AdcTheater.page
     )
 }
